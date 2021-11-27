731e193fb07a52991e5a94493825abe40ca16aa9
Kugayama Nana <nonamea.774@gmail.com>
2021-11-27 20:22:27 +0900

    add tests

--- test/handler.test.ts
+++ test/handler.test.ts
@@ -9,10 +9,24 @@ describe('handle', () => {
     jest.resetModules()
   })
 
-  test('handle GET', async () => {
-    const result = await handleRequest(new Request('/', { method: 'GET' }))
-    expect(result.status).toEqual(200)
-    const text = await result.text()
-    expect(text).toEqual('request method: GET')
+  test('redirect http', async () => {
+    const result = await handleRequest(new Request('http://nna774.net/', { method: 'GET' }))
+    expect(result.status).toEqual(301)
+    expect(result.headers.get('Location')).toEqual('https://nna774.net/')
+  })
+  test('redirect blog', async () => {
+    const result = await handleRequest(new Request('https://blog.nna774.net', { method: 'GET' }))
+    expect(result.status).toEqual(301)
+    expect(result.headers.get('Location')).toEqual('https://nna774.net/blog/')
+  })
+  test('redirect piet-89', async () => {
+    const result = await handleRequest(new Request('https://nna774.net/piet/C89Book.pdf', { method: 'GET' }))
+    expect(result.status).toEqual(302)
+    expect(result.headers.get('Location')).toEqual('https://github.com/nna774/C89Book2/releases/download/v1.0/C89Book_honban.pdf')
+  })
+  test('redirect piet-91', async () => {
+    const result = await handleRequest(new Request('https://nna774.net/piet/C91Book.pdf', { method: 'GET' }))
+    expect(result.status).toEqual(302)
+    expect(result.headers.get('Location')).toEqual('https://github.com/nna774/C91book/releases/download/1.0.1/C91Book.pdf')
   })
 })
